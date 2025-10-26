6e3717f210d2a864affb2fc4a04d171f8da64185
Bhairavi <bhandaririddhima8@gmail.com>
2025-10-26 02:26:32 +0000

    add decision tree sample prototype

---
+++ Prototype-2/Decision Tree Prototype.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct Node {
+    char question[200];
+    struct Node* yes;
+    struct Node* no;
+    
+};
+
+struct Node* createNode(char* question) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    strcpy(newNode->question, question);
+
+    newNode->yes = NULL;
+    newNode->no = NULL;
+
+    return newNode;
+}
+
+void traverse(struct Node* node) {
+    if(node==NULL)  return;
+
+    if(node->yes==NULL && node->no==NULL) {
+        printf("\n %s\n", node->question);
+        return;
+    }
+
+    char answer;
+    printf("\n%s (y/n): ", node->question);
+    scanf("%c", &answer);
+
+    if(answer=='y' || answer=='Y')
+        traverse(node->yes);
+    else
+        traverse(node->no);
+}
+
+int main() {
+    struct Node* root = createNode("Do you need your data in a specific order? ");
+    root->yes = createNode("Do you insert and remove from the same end? ");
+    root->no = createNode("Do you want random access? ");
+
+    root->yes->yes = createNode("Use a Stack (LIFO).");
+    root->yes->no = createNode("Use a Queue (FIFO).");
+
+    root->no->yes = createNode("Use an Array.");
+    root->no->no = createNode("Use a Linked List.");
+
+    traverse(root);
+
+    return 0;
+}
