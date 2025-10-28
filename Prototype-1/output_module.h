140238967cbaf42313af330e8becf56a901cd85b
Bhairavi <bhandaririddhima8@gmail.com>
2025-10-28 03:40:21 +0000

    Debug output display

--- Prototype-1/output_module.h
+++ Prototype-1/output_module.h
@@ -9,7 +9,7 @@ void showOutput(char recommendation[], char reason[])
 
     printf("\n=== Data Structure Recommendation ===\n");
     printf("Based on your input, we recommend the following:\n");
-    printf("Data Structure: %c\n", recommendation);
+    printf("Data Structure: %s\n", recommendation);
     printf("Reason: %s\n", reason);
     printf("Thank you for using our DS Advisor Demo system!\n");
 
